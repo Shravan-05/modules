bdc15dfa2e29c294370cffefcaee28d318777458
Shravan_007 <116939692+TECHNOtech2@users.noreply.github.com>
2024-01-17 20:17:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from './components/profile';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <Home/>
+  );
+};
+
+
+export default App;
