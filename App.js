ab30e5be6e511d9d986e0be96167a75043f0668e
Shravan Ghorpade <116939692+Shravan-05@users.noreply.github.com>
2024-01-21 16:11:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import YogaTracker from './Components/YogaTracker';
+export default function App() {
+  return (
+    <YogaTracker/>
+  );
+}
+
